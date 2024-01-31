dd7aa6f05bab84acce122afe58ea4c65d6d30b05
KunleTheDev <ikreezy@gmail.com>
2024-01-31 17:28:14 -0500

    Step 32. Adding the minus, multiply, and divide functions, fixing the error then pushing it to GitHub.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,6 +1,6 @@
 package travis_ci_tutorial_java;
 
-import java.util.*;
+//import java.util.*;
 
 import static org.junit.Assert.*;
 import org.junit.*;
@@ -16,5 +16,17 @@ public class SimpleCalculatorTest {
 	public void testSubtract() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
+	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(3, 5), 15);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 2), 5);
 	}
 }
